ef8f6604b8d59ea740f6c69cfa37f76f96a65d3d
0gnon <ognonjaune@gmail.com>
2025-07-16 12:10:48 +0200

    Deploy to Ognon

--- d.sh
+++ d.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Build l'application
 npm run build
-gh-pages -d dist -r git@github.com:Ognon/freep0nx.git
+
+# Déploie sur gh-pages
+npx gh-pages -d dist -r git@github.com:Ognon/freep0nx.git
+
+# Push les changements sur main
 git add .
 git commit -m "Deploy to Ognon"
 git push ognon main
